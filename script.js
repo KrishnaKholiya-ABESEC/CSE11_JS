9c9409f5ad30ee3079640e4866bea2e97f7ce5c2
Krishna Kholiya <krishnakholiya12@gmail.com>
2026-02-24 10:00:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num= undefined;
+console.log(num);
+console.log(typeof(num));
+num="Krishna";
+console.log(num);
+console.log(typeof(num));
