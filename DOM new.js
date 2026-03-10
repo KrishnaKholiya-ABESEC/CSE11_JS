ca3dcef77a39959c65be4d93c2a056c23ad66199
Krishna Kholiya <krishnakholiya12@gmail.com>
2026-03-10 10:32:11 +0530

    Add files via upload

---
+++ DOM new.js
@@ -0,0 +1,6 @@
+const test2=document.querySelectorAll("ul li");
+for (x in test2){
+    test2[x].style.backgroundColor = "green";
+    test2[x].style.margin = "10px";
+    test2[x].style.color = "blue";
+}
