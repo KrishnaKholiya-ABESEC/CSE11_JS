daa9364ce5cf87e8a5a43d278a7fe699bb9fbfc7
Krishna Kholiya <krishnakholiya12@gmail.com>
2026-03-10 09:39:40 +0530

    Add files via upload

---
+++ script12.js
@@ -0,0 +1,15 @@
+let collection = [
+    {},
+    [],
+    true,
+    "john",
+    function(){},
+    90009898193,
+    undefined,
+    null,
+    new String("abc"),
+    new Date(),
+];
+console.log(collection);
+collection.teacherName="John Mac";
+collection.phoneNo=950008193;
